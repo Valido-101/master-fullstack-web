577014db8a3c76859531a8bf02278cd207d15d78
jesus valido zafra <jesuszafra22@gmail.com>
2021-12-11 20:22:19 +0100

    Método de creación de nueva categoría creado y funcional

--- api-rest-laravel/app/Http/Controllers/CategoryController.php
+++ api-rest-laravel/app/Http/Controllers/CategoryController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Response;
 use App\Category;
 
 class CategoryController extends Controller {
+    
+    public function __construct() {
+        $this->middleware('api.auth', ['except' => ['index','show']]);
+    }
 
     public function index() {
         //Devolvemos todas las categorías (Select * from categories)
@@ -37,6 +41,53 @@ class CategoryController extends Controller {
             );
         }
 
+        return response()->json($data, $data['code']);
+    }
+    
+    public function store(Request $request){
+        //Recoger los datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+        
+        if (!empty($params_array)) {
+            
+            //Validar los datos
+        $validate = \Validator::make($params_array, [
+            'name' => 'required'
+        ]);
+        
+        //Guardar la categoría
+        if ($validate->fails()) {
+            
+            $data = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No se ha guardado la categoría'
+            ];
+            
+        }else{
+            $category = new Category();
+            $category->name = $params_array['name'];
+            $category->save();
+            
+            $data = [
+                'code' => 200,
+                'status' => 'success',
+                'category' => $category
+            ];
+        }
+            
+        }else{
+            
+            $data = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No se ha enviado una categoría'
+            ];
+            
+        }
+        
+        //Devolver resultado
         return response()->json($data, $data['code']);
     }
 
