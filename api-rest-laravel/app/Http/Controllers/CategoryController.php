aeee76d3e6c6db01ac6951b86274d0c9103939fd
jesus valido zafra <jesuszafra22@gmail.com>
2021-12-11 20:38:53 +0100

    Método para modificar categorías creado y funcional

--- api-rest-laravel/app/Http/Controllers/CategoryController.php
+++ api-rest-laravel/app/Http/Controllers/CategoryController.php
@@ -90,5 +90,59 @@ class CategoryController extends Controller {
         //Devolver resultado
         return response()->json($data, $data['code']);
     }
+    
+    public function update($id, Request $request){
+        
+        //Recoger datos que llegan por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+        
+        
+        if (!empty($params_array)) {
+            
+            //Validar datos
+            $validate = \Validator::make($params_array, [
+                'name' => 'required'
+            ]);
+        
+            if ($validate->fails()) {
+                
+                $data = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No se ha enviado un nombre'
+                ];
+                
+            }else{
+                
+                //Quitar lo que no quiero actualizar
+                unset($params_array['id']);
+                unset($params_array['created_at']);
+
+                //Actualizar el registro (categoría)
+                $category = Category::where('id', $id)->update($params_array);
+
+                $data = [
+                    'code' => 200,
+                    'status' => 'success',
+                    'category' => $params_array
+                ];
+
+            }
+            
+        }else{
+            
+            $data = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No se ha enviado una categoría'
+            ];
+            
+        }
+        
+        //Devolver respuesta
+        return response()->json($data, $data['code']);
+        
+    }
 
 }
